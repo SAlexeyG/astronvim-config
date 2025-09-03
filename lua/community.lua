6b37e29740e82357d1e9c82f582cc99a392518fc
Alexey Smyshlyaev <asm@highcore.io>
2025-09-03 15:57:22 +0300

    Lua added

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.cs-omnisharp" },
   -- import/override with your plugins folder
